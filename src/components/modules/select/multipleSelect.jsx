fec0716f0cb082a8805020d1ae2377801c335aa0
hyoeun kwon <tenstreet@naver.com>
2025-05-02 17:58:31 +0900

    style-버튼 칩태그로 변경

--- src/components/modules/select/multipleSelect.jsx
+++ src/components/modules/select/multipleSelect.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useState, useRef } from 'react';
-import { Select, MenuItem, ListItemText, FormControl } from '@mui/material';
+import { Select, MenuItem, ListItemText, FormControl, OutlinedInput, Chip } from '@mui/material';
 import Label from '@components/modules/label/Label';
 
-const itemStyle = {
-  display: 'inline-flex',
-  alignItems: 'center',
-  padding: '4px 8px',
-  borderRadius: '12px',
-  backgroundColor: '#007bff',
-  color: 'white',
-  fontSize: '14px',
-  fontWeight: '500',
-  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-  whiteSpace: 'nowrap',
-  lineHeight: '14px',
-};
+// const itemStyle = {
+//   display: 'inline-flex',
+//   alignItems: 'center',
+//   padding: '3px 8px',
+//   borderRadius: '3px',
+//   backgroundColor: 'rgb(36 142 255)',
+//   backgroundColor: 'red',
+//   color: 'white',
+//   fontSize: '14px',
+//   fontWeight: '500',
+//   marginRight: '5px',
+//   boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+//   whiteSpace: 'nowrap',
+//   lineHeight: '14px',
+// };
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -32,11 +34,12 @@ const MultipleSelect = (props) => {
   const [value, setValue] = useState([]);
   const useEffect_0001 = useRef(false);
   useEffect(() => {
-    if (process.env.NODE_ENV === 'development') {  //process.env.NODE_ENV === 'development'
-      if (!useEffect_0001.current){
+    if (process.env.NODE_ENV === 'development') {
+      //process.env.NODE_ENV === 'development'
+      if (!useEffect_0001.current) {
         useEffect_0001.current = true;
-        return; 
-      } 
+        return;
+      }
     }
     if (initValue) {
       setValue(initValue);
@@ -55,8 +58,9 @@ const MultipleSelect = (props) => {
 
   return (
     <Label required={required} label={label} labelBackgroundFlag>
-      <FormControl fullWidth>
-        <Select
+      <FormControl>
+        {/* <Select
+          sx={{ height: '30px', width: '400px' }}
           multiple
           MenuProps={MenuProps}
           name={name}
@@ -71,6 +75,40 @@ const MultipleSelect = (props) => {
                   <span key={item.id} style={itemStyle}>
                     {item.name}
                   </span>
+                )
+              );
+            })
+          }
+        >
+          {dataList.map((option) => (
+            <MenuItem key={option.id} value={option.id}>
+              <ListItemText primary={option.name} />
+            </MenuItem>
+          ))}
+        </Select> */}
+        <Select
+          sx={{ height: '30px', width: '400px' }}
+          id="demo-multiple-chip"
+          multiple
+          MenuProps={MenuProps}
+          name={name}
+          value={value}
+          onChange={handleChange}
+          input={<OutlinedInput id="select-multiple-chip" placeholder="Chip" />}
+          renderValue={(selected) =>
+            selected.map((data) => {
+              const item = dataList.find((option) => option.id === data);
+
+              return (
+                item && (
+                  <Chip
+                    key={item.id}
+                    label={item.name}
+                    variant="light"
+                    color="primary"
+                    size="small"
+                    sx={{ marginRight: '5px' }}
+                  />
                 )
               );
             })
